28783005700069240e1f19ba71109f4a52a972a3
Gintautas Miselis <gintautas@miselis.lt>
2016-01-30 23:25:24 +0000

    Upgraded ZendExpressive to 1.0.0

--- config/autoload/middleware-pipeline.global.php
+++ config/autoload/middleware-pipeline.global.php
@@ -10,31 +10,44 @@ return [
         ],
     ],
 
-    // This can be used to seed pre- and/or post-routing middleware
     'middleware_pipeline' => [
-        // An array of middleware to register prior to registration of the
-        // routing middleware
-        'pre_routing' => [
-            [ 'middleware' => Helper\ServerUrlMiddleware::class ],
-            //[
-            // Required:
-            //    'middleware' => 'Name of middleware service, or a callable',
-            // Optional:
-            //    'path'  => '/path/to/match',
-            //    'error' => true,
-            //],
+        // An array of middleware to pipe to the application.
+        // Each item is of the following structure:
+        // [
+        //     // Required:
+        //     'middleware' => 'Name or array of names of middleware services and/or callables',
+        //     // Optional:
+        //     'path'  => '/path/to/match',
+        //     'error' => true,
+        // ],
+        [
+            'middleware' => [
+                Helper\ServerUrlMiddleware::class,
+            ],
+            'priority' => PHP_INT_MAX,
         ],
 
-        // An array of middleware to register after registration of the
-        // routing middleware
-        'post_routing' => [
-            //[
-            // Required:
-            //    'middleware' => 'Name of middleware service, or a callable',
-            // Optional:
-            //    'path'  => '/path/to/match',
-            //    'error' => true,
-            //],
-        ],
+        // The following is an entry for:
+        // - routing middleware
+        // - middleware that reacts to the routing results
+        // - dispatch middleware
+        [
+            'middleware' => [
+                Zend\Expressive\Container\ApplicationFactory::ROUTING_MIDDLEWARE,
+//                    Helper\UrlHelperMiddleware::class,
+                Zend\Expressive\Container\ApplicationFactory::DISPATCH_MIDDLEWARE,
+            ],
+            'priority' => 1,
+        ]
+
+        // The following is an entry for the dispatch middleware:
+
+        // Place error handling middleware after the routing and dispatch
+        // middleware, with negative priority.
+        // [
+        //     'middleware' => [
+        //     ],
+        //     'priority' => -1000,
+        // ],
     ],
 ];
