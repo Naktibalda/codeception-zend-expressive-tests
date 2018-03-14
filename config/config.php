48a14ed7c7be2adf0bcedf1ace16bc5a6af27891
Gintautas Miselis <naktibalda@gmail.com>
2018-03-14 09:15:35 +0000

    Updated config.php

--- config/config.php
+++ config/config.php
@@ -1,38 +1,28 @@
 <?php
 
-use Zend\Stdlib\ArrayUtils;
-use Zend\Stdlib\Glob;
-
-/**
- * Configuration files are loaded in a specific order. First ``global.php`` and afterwards ``local.php``. This way
- * local settings overwrite global settings.
- *
- * The configuration can be cached. This can be done by setting ``config_cache_enabled`` to ``true``.
- *
- * The configuration is stored in json so it is not depended on 3rd party libraries. Feel free to use something else
- * like Zend\Config\Writer to write PHP arrays.
- *
- * Obviously, if you use closures in your config you can't cache it.
- */
-
-$cachedConfigFile = 'data/cache/app_config.php';
-
-$config = [];
-if (is_file($cachedConfigFile)) {
-    // Try to load the cached config
-    $config = json_decode(file_get_contents($cachedConfigFile), true);
-} else {
-    // Load configuration from autoload path
-    foreach (Glob::glob('config/autoload/{{,*.}global,{,*.}local}.php', Glob::GLOB_BRACE) as $file) {
-        $config = ArrayUtils::merge($config, include $file);
-    }
-
-    // Cache config if enabled
-    if (isset($config['config_cache_enabled']) && $config['config_cache_enabled'] === true) {
-        file_put_contents($cachedConfigFile, json_encode($config));
-    }
-}
-
-// Return an ArrayObject so we can inject the config as a service in Aura.Di
-// and still use array checks like ``is_array``.
-return new \ArrayObject($config, \ArrayObject::ARRAY_AS_PROPS);
+use Zend\ConfigAggregator\ArrayProvider;
+use Zend\ConfigAggregator\ConfigAggregator;
+use Zend\ConfigAggregator\PhpFileProvider;
+// To enable or disable caching, set the `ConfigAggregator::ENABLE_CACHE` boolean in
+// `config/autoload/local.php`.
+$cacheConfig = [
+    'config_cache_path' => 'data/config-cache.php',
+];
+$aggregator = new ConfigAggregator([
+    \Zend\Expressive\ConfigProvider::class,
+    \Zend\Expressive\Router\ConfigProvider::class,
+    // Include cache configuration
+    new ArrayProvider($cacheConfig),
+    // Default App module config
+    App\ConfigProvider::class,
+    // Load application config in a pre-defined order in such a way that local settings
+    // overwrite global settings. (Loaded as first to last):
+    //   - `global.php`
+    //   - `*.global.php`
+    //   - `local.php`
+    //   - `*.local.php`
+    new PhpFileProvider(realpath(__DIR__) . '/autoload/{{,*.}global,{,*.}local}.php'),
+    // Load development config if it exists
+    new PhpFileProvider(realpath(__DIR__) . '/development.config.php'),
+], $cacheConfig['config_cache_path']);
+return $aggregator->getMergedConfig();
