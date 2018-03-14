a2f6c6c5456dae92379bfcb39eb1733e4d782369
Gintautas Miselis <naktibalda@gmail.com>
2018-03-14 10:03:32 +0000

    Removed App\ConfigProvider from config.php

--- config/config.php
+++ config/config.php
@@ -13,8 +13,6 @@ $aggregator = new ConfigAggregator([
     \Zend\Expressive\Router\ConfigProvider::class,
     // Include cache configuration
     new ArrayProvider($cacheConfig),
-    // Default App module config
-    App\ConfigProvider::class,
     // Load application config in a pre-defined order in such a way that local settings
     // overwrite global settings. (Loaded as first to last):
     //   - `global.php`
